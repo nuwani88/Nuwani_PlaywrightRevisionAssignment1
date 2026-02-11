40d53f27e422f9617e9babda3bca07688e8d6e96
nuwani88 <nuvanin@zone24x7.com>
2026-02-12 03:13:50 +0530

    Add files via upload
    Files Uploaded

---
+++ playwright.config.ts
@@ -0,0 +1,47 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  
+  testDir: './tests',
+  fullyParallel: true,
+  timeout: 60000,
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox', // This name must match exactly what you type in the terminal
+      use: { ...devices['Desktop Firefox'] },
+    },
+  ],
+
+  retries: 1 ,
+  
+  reporter: 'html',
+
+  use: {
+    baseURL: 'https://practicesoftwaretesting.com',
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    navigationTimeout: 45000,
+    
+    launchOptions: {
+      headless: false,
+      slowMo: 500
+    },
+
+    
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { browserName: 'chromium' },
+    },
+  ],
+});
+
+
